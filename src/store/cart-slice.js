cbb512e99b49900afb656332ef1184a2dbdd34fe
akhileshchamoli05 <chamoliakhilesh71@gmail.com>
2023-08-15 09:31:33 +0530

    first commit

---
+++ src/store/cart-slice.js
@@ -0,0 +1,119 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { uiActions } from './ui-slice';
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+      }
+    },
+  },
+});
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!',
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        'https://expense-tracker-8f63f-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      
+      }
+      console.log(response);
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      );
+    }
+  };
+};
+
+export const cartActions = cartSlice.actions;
+// cartActions.js
+// cart-actions.js
+// cart-actions.js
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch('https://expense-tracker-8f63f-default-rtdb.firebaseio.com/cart.json'); // Adjust the URL as needed
+      if (!response.ok) {
+        throw new Error('Failed to fetch cart data.');
+      }
+
+      const cartData = await response.json();
+      console.log(cartData);
+      dispatch(cartActions.replaceCart(cartData));
+      
+    } catch (error) {
+      dispatch({ type: 'FETCH_CART_DATA_FAILURE', payload: error.message });
+    }
+  };
+};
+
+
+
+
+export default cartSlice;
